f1ed2f89263a41d7871f774f09ac94b6cea816ae
4319 Задорожный А.В <zvvqwerty1@gmail.com>
2025-02-01 17:57:49 +0800

    class Post

---
+++ app/models/post.py
@@ -0,0 +1,15 @@
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import mapped_column
+from sqlalchemy import Text, func
+from social_network.app.models.base import Base
+
+
+
+class Post(Base):
+    __tablename__ = "posts"
+    id = mapped_column(primary_key=True)
+    content = mapped_column(Text)
+    media_url = mapped_column(nullable=True)
+    author_id = mapped_column(ForeignKey("users.id"))
+    created_at = mapped_column(server_default=func.now())
+
