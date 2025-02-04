0c0b39fc25f799d59282248c01d5b184b8ec7103
4319 Емельянов А.М. <andrey_emelyanov_2005@mail.ru>
2025-02-04 18:26:05 +0300

    fixed alembic

--- app/database.py
+++ app/database.py
@@ -4,8 +4,8 @@ from app.config import settings
 
 
 
-async_engine = create_async_engine(url=settings.DATABASE_URL, echo=True)
-async_session_maker = async_sessionmaker(async_engine, expire_on_commit=False)
+async_engine = create_async_engine(url=settings.DATABASE_URL, echo=True, pool_pre_ping=True)
+async_session_maker = async_sessionmaker(bind=async_engine, expire_on_commit=False)
 
 class Base(DeclarativeBase):
     pass
