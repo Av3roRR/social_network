81777dbf70ae1cf8df9eb58795193cf1c22414a5
4319 Задорожный А.В <zvvqwerty1@gmail.com>
2025-02-09 00:53:15 +0300

    comment для модели добавил счетчик лайков,пока не тестил

--- app/models/comment.py
+++ app/models/comment.py
@@ -1,8 +1,10 @@
-from app.database import Base
-from sqlalchemy.orm import mapped_column, Mapped, relationship
-from sqlalchemy import Text, ForeignKey, func
-from datetime import datetime
+from select import select
 
+from app.database import Base
+from sqlalchemy.orm import mapped_column, Mapped, relationship, column_property
+from sqlalchemy import Text, ForeignKey, func, select, where
+from datetime import datetime
+from app.models.like import Like
 
 class Comment(Base):
     __tablename__ = "comments"
@@ -16,3 +18,8 @@ class Comment(Base):
     author = relationship("app.models.user.User", back_populates="comments")
     post = relationship("app.models.post.Post", back_populates="comments")
     likes = relationship("app.models.like.Like", back_populates="comment")  # Исправил
+
+    #лайки для комментариев
+    likes_count = column_property(
+        select(func.count(Like.id)).where(Like.comment_id == id).scalar_subquery()
+    )
