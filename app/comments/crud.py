ac7d791c572e253b7c003975cab0c14c5e1bec6e
4319 Задорожный А.В <zvvqwerty1@gmail.com>
2025-02-09 14:36:24 +0300

    комментарии к посту

--- app/comments/crud.py
+++ app/comments/crud.py
@@ -24,4 +24,13 @@ async def get_comment(db:AsyncSession, comment_id: int):
                              )
     return result.scalar_one_or_none()
 
-
+async def get_comments_for_post(db:AsyncSession, post_id: int, skip: int = 0, limit: int = 100):
+    result = await db.execute(
+        select(Comment)
+        .options(selectinload(Comment.author),selectinload(Comment.likes_count))
+        .where(Comment.post_id == post_id)
+        .order_by(Comment.created_at.desc())
+        .offset(skip)
+        .limit(limit)
+    )
+    return result.scalars().all()
