9fa53d683026d50d584352c6aabf4537b21c803c
4319 Задорожный А.В <zvvqwerty1@gmail.com>
2025-02-09 14:24:59 +0300

    crud for get_comment

--- app/comments/crud.py
+++ app/comments/crud.py
@@ -1,18 +1,27 @@
+from sqlalchemy import select
 
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.models.comment import Comment
 from app.comments.schemas import CommentCreate
-
+from sqlalchemy.orm import selectinload
 
 async def create_comment(db:AsyncSession, comment_data:CommentCreate, user_id: int, post_id: int):
     comment = Comment(
         text = comment_data.text,
-        author_id = user_id,\
+        author_id = user_id,
         post_id = post_id
     )
     db.add(comment)
     await db.commit()
     await db.refresh(comment)
     return comment
+
+async def get_comment(db:AsyncSession, comment_id: int):
+    result = await db.execute(
+        select(Comment)
+        .options(selectinload(Comment.author))
+        .where(Comment.id == comment_id)
+                             )
+    return result.scalar_one_or_none()
 
 
