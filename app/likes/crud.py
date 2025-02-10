a46cbde50507653206c7995210760d6a668079ec
4319 Задорожный А.В <zvvqwerty1@gmail.com>
2025-02-10 23:58:38 +0300

    лайк удален

--- app/likes/crud.py
+++ app/likes/crud.py
@@ -1,8 +1,10 @@
 
 from fastapi import HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
+from sqlalchemy import select, selectinload, delete
 from app.models.like import Like
+
+
 async def create_like(db: AsyncSession, user_id: int, post_id: int|None = None, comment_id: int|None = None):
     existing_like = await db.execute(
         select(Like)
@@ -22,4 +24,24 @@ async def create_like(db: AsyncSession, user_id: int, post_id: int|None = None, 
     await db.commit()
     await db.refresh(like)
     return like
+
+
+async def get_like(db: AsyncSession, like_id: int):
+    result = await db.execute(
+        select(Like)
+        .options(selectinload(Like.user_id))
+        .where(Like.id == like_id)
+    )
+    return result.scalar_one_or_none()
+
+async def delete_like(db: AsyncSession, like_id: int, user_id: int):
+    like = await get_like(db,like_id)
+    if not like:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail="лайк не обнаружен")
+    if like.user_id != user_id:
+        raise HTTPException(status.HTTP_403_FORBIDDEN, detail="нельзя удалить чужой like")
+    await db.delete(like)
+    await db.commit()
+    return {"message":"лайк удален"}
+
 
